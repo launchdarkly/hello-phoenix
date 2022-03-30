46cdfbea28066b1554122a809d954427e01afcb1
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-03-30 15:29:50 -0700

    Remove unused file. Get the example working with a liveview in a recent version of Phoenix.

--- lib/hello_phoenix_web/controllers/hello_launchdarkly_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule HelloPhoenixWeb.HelloLaunchdarklyController do
-  use HelloPhoenixWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- lib/hello_phoenix_web/views/hello_launchdarkly_live.ex
+++ lib/hello_phoenix_web/views/hello_launchdarkly_live.ex
@@ -3,21 +3,34 @@ defmodule HelloPhoenixWeb.HelloLaunchdarklyLive do
 #  use Phoenix.LiveView
 
   def render(assigns) do
-    ~H"""
-    Alternate Enabled: <%= @alternate %>
-    """
+    case assigns.alternate do
+      true ->
+        ~H"""
+          <div class="phx-hero">
+          <h2>Hello <%= @user %>, from LaunchDarkly!</h2>
+          <p>Welcome to the NEW cool page, enabled by LaunchDarkly!</p>
+          </div>
+        """
+      false ->
+        ~H"""
+          <div class="phx-hero">
+          <h2>Hello <%= @user %>, from LaunchDarkly!</h2>
+          </div>
+        """
+    end
+
   end
 
   def mount(%{"user" => user}, _params, socket) do
     if connected?(socket), do: Process.send_after(self(), {:update, user}, 30000)
 
     alternate = :ldclient.variation("alternate.page", %{:key => user}, false)
-    {:ok, assign(socket, :alternate, alternate)}
+    {:ok, assign(socket, %{:alternate => alternate, :user => user})}
   end
 
   def handle_info({:update, user}, socket) do
     Process.send_after(self(), {:update, user}, 30000)
     alternate = :ldclient.variation("alternate.page", %{:key => user}, false)
-    {:noreply, assign(socket, :alternate, alternate)}
+    {:noreply, assign(socket, %{:alternate => alternate, :user => user})}
   end
 end
